8023c528bfe07762a4b31d39e55e9fc9d120d173
DaviCarvalhoo <davicarvalhotech@gmail.com>
2024-07-14 19:34:19 -0300

    add files spring

---
+++ src/primeiros-passo/src/test/java/com/davi/primeiros_passo/PrimeirosPassoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.primeiros_passo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeirosPassoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
